5516162c603bdbd4f324ba97a645b12f6db2b3ed
Ysmael Marinho <ysmaeljunior@gmail.com>
2020-10-15 12:03:37 -0300

    initial commit

---
+++ initial/src/test/java/marketing/mailshots/MailshotsFixture.java
@@ -0,0 +1,9 @@
+package marketing.mailshots;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class MailshotsFixture {
+
+}
